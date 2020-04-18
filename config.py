91ababfd94a3655ceda4d92bdba0b6123dd9c9d1
brendan <bsavelli66@gmail.com>
2020-04-18 06:41:32 +0000

    config

--- config.py
+++ config.py
@@ -7,6 +7,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_USERNAME = os.environ.get('bsavelli66@gmail.com')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-    
